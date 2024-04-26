ebba5344730c124e947101c49d66398baada1551
Njuguna Mmwaura <coder866@gmail.com>
2024-04-26 17:43:42 +0300

    WIP-2024-04-26

---
+++ config/laravel-twitter-streaming-api.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+     * To work with Twitter's Streaming API you'll need some credentials.
+     *
+     * If you don't have credentials yet, head over to https://developers.twitter.com/
+     */
+
+    'handle' => env('TWITTER_HANDLE'),
+
+    'api_key' => env('TWITTER_API_KEY'),
+
+    'api_secret_key' => env('TWITTER_API_SECRET_KEY'),
+
+    'bearer_token' => env('TWITTER_BEARER_TOKEN'),
+];
